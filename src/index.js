8da36c384c6df41c6bb07f9b5ee3b15d8d87853d
lk3v1nn <87085706+lk3v1nn@users.noreply.github.com>
2023-11-02 18:21:23 -0600

    Update cors

--- src/index.js
+++ src/index.js
@@ -8,18 +8,10 @@ const DB_UMG = require("./baseDeDatos");
 
 const app = express();
 
-/*app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://proyecto-2-pi.vercel.app/"); // Es lo mismo que la configuracion de CORS de abajo
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});*/
-
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://proyecto-2-pi.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
