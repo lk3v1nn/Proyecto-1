15d203ce8bf874c7706bce728c931b547df4c5d9
lk3v1nn <87085706+lk3v1nn@users.noreply.github.com>
2023-11-02 18:46:51 -0600

    Update rutasUsuario.js

--- src/controllers/rutasUsuario.js
+++ src/controllers/rutasUsuario.js
@@ -74,7 +74,9 @@ router.post("/api/login", async (req, res) => {
         const token = jwt.sign({ id: data._id }, "textosupersecreto", {
             expiresIn: 60 * 60 * 24 * 30,
         });
-        res.cookie("token", token).json({ ok: 'sesion iniciada' });
+        res.cookie("token", token);
+        console.log(token);
+    //    res.cookie("token", token).json({ ok: 'sesion iniciada' });
     } catch (error) {
         res.status(500).json({ Error: "Error del servidor" });
     }
